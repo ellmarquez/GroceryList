7799b16554756e78cd080568db50e0f4dccfdb1d
Ell Marquez <ellstripes@gmail.com>
2018-05-14 12:01:03 -0500

    Updated app

--- Glistapp.py
+++ Glistapp.py
@@ -1,8 +1,8 @@
 
 print ("""Enter A to add to your list,
-enter P to print your list,
-enter DELETE to clear your list,
-leave blank and press enter to exit""") 
+Enter P to print your list,
+Enter DELETE to clear your list,
+Leave blank and press enter to exit""") 
 
 userinput= input()
 userinput =userinput.lower()
@@ -10,12 +10,15 @@ userinput =userinput.lower()
 if userinput == 'p':
      with open("grocerylist.txt") as temp:
         list=temp.read()
-        print (list)
+        print ("--------\n" + list)
 elif userinput== 'a':
    print ("What would you like to add?")
    glist=input()
    with open("grocerylist.txt", "a") as wtemp:
-       wtemp.write(glist)
+       wtemp.write(glist + "\n")
+   with open("grocerylist.txt") as temp:
+        list=temp.read()
+        print ("--------\n"+ list)
 elif userinput =='delete':
     print ("Your list has been deleted.")
     open('grocerylist.txt','w').close() 
